d2150c188aec9839a1a21c7f0fa21131ff61873f
ema <mccoy39082@163.com>
2026-03-27 03:16:27 +0800

    Add window blur helper and disable on load #2

--- QuickLook.Plugin.SumatraPDFReader/SumatraPanel.cs
+++ QuickLook.Plugin.SumatraPDFReader/SumatraPanel.cs
@@ -19,9 +19,11 @@ using QuickLook.Common.Helpers;
 using QuickLook.Common.Plugin;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using System.Windows.Resources;
 
 namespace QuickLook.Plugin.SumatraPDFReader;
@@ -34,7 +36,16 @@ public partial class SumatraPanel : UserControl
     {
         InitializeComponent();
         presenter.Background = System.Windows.Media.Brushes.White;
+        Loaded += SumatraPanel_Loaded;
         Unloaded += SumatraPanel_Unloaded;
+    }
+
+    private void SumatraPanel_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (Window.GetWindow(this) is Window win)
+        {
+            WindowHelper.DisableBlur(win);
+        }
     }
 
     private void SumatraPanel_Unloaded(object sender, RoutedEventArgs e)
@@ -64,4 +75,153 @@ public partial class SumatraPanel : UserControl
         _sumatraPDFControl.LoadFile(path);
         presenter.Child = _sumatraPDFControl;
     }
+}
+
+file static class WindowHelper
+{
+    public static void EnableBlur(Window window)
+    {
+        var accent = new AccentPolicy();
+        var accentStructSize = Marshal.SizeOf(accent);
+        accent.AccentState = AccentState.AccentEnableBlurbehind;
+        accent.AccentFlags = 2;
+        accent.GradientColor = 0x99FFFFFF;
+
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+
+        var data = new WindowCompositionAttributeData
+        {
+            Attribute = WindowCompositionAttribute.WcaAccentPolicy,
+            SizeOfData = accentStructSize,
+            Data = accentPtr
+        };
+
+        User32.SetWindowCompositionAttribute(new WindowInteropHelper(window).Handle, ref data);
+
+        Marshal.FreeHGlobal(accentPtr);
+    }
+
+    public static void DisableBlur(Window window)
+    {
+        var accent = new AccentPolicy();
+        var accentStructSize = Marshal.SizeOf(accent);
+        accent.AccentState = AccentState.AccentDisabled;
+
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+
+        var data = new WindowCompositionAttributeData
+        {
+            Attribute = WindowCompositionAttribute.WcaAccentPolicy,
+            SizeOfData = accentStructSize,
+            Data = accentPtr
+        };
+
+        var hwnd = new WindowInteropHelper(window).EnsureHandle();
+        User32.SetWindowCompositionAttribute(hwnd, ref data);
+
+        Marshal.FreeHGlobal(accentPtr);
+
+        var margins = new Dwmapi.Margins(0, 0, 0, 0);
+        Dwmapi.DwmExtendFrameIntoClientArea(hwnd, ref margins);
+
+        if (Environment.OSVersion.Version >= new Version(10, 0, 22523)) // Windows 11 build 22523 or later
+        {
+            var backdropType = (int)Dwmapi.SystembackdropType.None;
+            Dwmapi.DwmSetWindowAttribute(hwnd, (uint)Dwmapi.WindowAttribute.SystembackdropType, ref backdropType, Marshal.SizeOf(typeof(int)));
+        }
+        else if (Environment.OSVersion.Version >= new Version(10, 0, 21996)) // Windows 11 build 21996 or later
+        {
+            var micaEnabled = 0;
+            Dwmapi.DwmSetWindowAttribute(hwnd, (uint)Dwmapi.WindowAttribute.MicaEffect, ref micaEnabled, Marshal.SizeOf(typeof(int)));
+        }
+
+        if (Environment.OSVersion.Version >= new Version(10, 0, 21996)) // Windows 11 build 21996 or later
+        {
+            var cornerPreference = (int)Dwmapi.WindowCornerPreference.Round;
+            Dwmapi.DwmSetWindowAttribute(hwnd, (uint)Dwmapi.WindowAttribute.WindowCornerPreference, ref cornerPreference, Marshal.SizeOf(typeof(int)));
+        }
+    }
+
+    public enum WindowCompositionAttribute
+    {
+        WcaAccentPolicy = 19,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public nint Data;
+        public int SizeOfData;
+    }
+
+    private enum AccentState
+    {
+        AccentDisabled = 0,
+        AccentEnableGradient = 1,
+        AccentEnableTransparentgradient = 2,
+        AccentEnableBlurbehind = 3,
+        AccentInvalidState = 4,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public int AccentFlags;
+        public uint GradientColor;
+        public readonly int AnimationId;
+    }
+}
+
+file static class User32
+{
+    [DllImport("user32.dll")]
+    public static extern int SetWindowCompositionAttribute(nint hwnd,
+        ref WindowHelper.WindowCompositionAttributeData data);
+}
+
+file static class Dwmapi
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Margins(int cxLeftWidth, int cxRightWidth, int cyTopHeight, int cyBottomHeight)
+    {
+        public int cxLeftWidth = cxLeftWidth;
+        public int cxRightWidth = cxRightWidth;
+        public int cyTopHeight = cyTopHeight;
+        public int cyBottomHeight = cyBottomHeight;
+    }
+
+    public enum WindowAttribute
+    {
+        UseImmersiveDarkMode = 20,
+        WindowCornerPreference = 33,
+        SystembackdropType = 38,
+        MicaEffect = 1029,
+    }
+
+    public enum WindowCornerPreference
+    {
+        Default = 0,
+        DoNotRound = 1,
+        Round = 2,
+        RoundSmall = 3,
+    }
+
+    public enum SystembackdropType
+    {
+        Auto = 0,
+        None = 1,
+        MainWindow = 2,
+        TransientWindow = 3,
+        TabbedWindow = 4,
+    }
+
+    [DllImport("DwmApi.dll")]
+    public static extern int DwmExtendFrameIntoClientArea(nint hwnd, ref Margins pMarInset);
+
+    [DllImport("dwmapi.dll")]
+    public static extern int DwmSetWindowAttribute(nint hwnd, uint dwAttribute, ref int pvAttribute, int cbAttribute);
 }
